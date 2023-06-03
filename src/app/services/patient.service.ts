b0bec7f44567915a9e2b61dd9674d02ca497e151
Uma Das <umaprasaddas1999@gmail.com>
2023-06-03 13:05:41 +0530

    new push

--- src/app/services/patient.service.ts
+++ src/app/services/patient.service.ts
@@ -1,36 +1,31 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Patient } from '../model/patient.model';
 import { Vaccine } from '../model/vaccine.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PatientService {
+  baseURL: string | undefined;
 
-  baseURL: string;
-
-  constructor(private http : HttpClient) { 
-    this.baseURL = environment.JsonServerURL;
+  constructor(private http: HttpClient) {
+    // this.baseURL = environment.JsonServerURL;
   }
-  addPatient(patient : Patient): Observable<any>{
-
-    return this.http.post(`${this.baseURL}/patients`,patient);
-
+  addPatient(patient: Patient): Observable<any> {
+    return this.http.post(`${this.baseURL}/patients`, patient);
   }
-  addVaccine(vaccine : Vaccine): Observable<any>{
-
-    return this.http.post(`${this.baseURL}/vaccines`,vaccine);
-
+  addVaccine(vaccine: Vaccine): Observable<any> {
+    return this.http.post(`${this.baseURL}/vaccines`, vaccine);
   }
 
-  getPatient():Observable<Patient[]>{
-    return this.http.get<Patient[]>(`${this.baseURL}/patients`)
+  getPatient(): Observable<Patient[]> {
+    return this.http.get<Patient[]>(`${this.baseURL}/patients`);
   }
 
-  getVaccine():Observable<Vaccine[]>{
-    return this.http.get<Vaccine[]>(`${this.baseURL}/vaccines`)
+  getVaccine(): Observable<Vaccine[]> {
+    return this.http.get<Vaccine[]>(`${this.baseURL}/vaccines`);
   }
 }
